d8f3cac6b6d10dfcd5677c84ec4e2814359f105c
AndrewDongminYoo <ydm2790@gmail.com>
2021-11-20 17:17:50 +0900

    assets

--- src/App.js
+++ src/App.js
@@ -1,21 +1,49 @@
 import React from 'react';
 import { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { AppLoading } from 'expo-app-loading';
+import AppLoading from 'expo-app-loading';
 import * as Font from 'expo-font';
 import { StyleSheet, Text, View, Image } from 'react-native';
 import { ThemeProvider } from '@react-navigation/native';
+import { FontAwesome } from '@expo/vector-icons';
 import { theme } from './theme';
+
+function cacheImages(images) {
+  return images.map(image => {
+    if (typeof image === 'string') {
+      return Image.prefetch(image);
+    } else {
+      return Asset.fromModule(image).downloadAsync();
+    }
+  });
+}
+
+function cacheFonts(fonts) {
+  return fonts.map(font => Font.loadAsync(font));
+}
 
 export default function App() {
 
   const [isReady, setIsReady] = useState(false);
 
   const _loadAssets = async () => {
-    const imageAssets = cacheImages([require('../assets/splash.png')]);
-    const fontAssets = cacheFonts([require('../assets/fonts/NotoSansKR-Regular.otf')]);
+    const imageAssets = cacheImages([
+      require('../assets/splash.png'),
+      require('../assets/logo.png'),
+      require('../assets/icon.png'),
+      require('../assets/graphic.png')
+    ]);
+    const fontAssets = cacheFonts([
+      FontAwesome.font,
+      require('../assets/fonts/NotoSansKR-Regular.otf'),
+      require('../assets/fonts/BlackHanSans-Regular.ttf'),
+      require('../assets/fonts/NotoSansKR-Bold.otf'),
+      require('../assets/fonts/NotoSansKR-Medium.otf')
+    ]);
     await Promise.all([...imageAssets, ...fontAssets]);
   }
+
+
 
   return isReady ? (
     <ThemeProvider theme={theme}>
